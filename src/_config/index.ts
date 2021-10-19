0f4a4c2ef03b746a94f566d8ae0b03ff8df7f121
Tim Welch <tim.j.welch@gmail.com>
2021-10-19 16:14:43 -0700

    fix bucket url

--- src/_config/index.ts
+++ src/_config/index.ts
@@ -1,7 +1,6 @@
 /**
  * Area of ocean within eez minus land in square miles. Calculated by drawing
- * sketch around hawaiian islands in original seasketch project, exporting the
- * resulting sketch, calling turf/area function on it and converting square
+ * sketch in seasketch project, exporting the resulting sketch, calling turf/area function on it and converting square
  * meters to square miles */
 export const STUDY_REGION_AREA_SQ_METERS = 465737168307.9038;
 export const STUDY_REGION_AREA_SQ_KM = STUDY_REGION_AREA_SQ_METERS / 1000;
@@ -12,7 +11,7 @@ export const units = "imperial";
 export const dataBucketUrl =
   process.env.NODE_ENV === "test"
     ? `http://127.0.0.1:8080/`
-    : `https://gp-hawaii-reports-next-datasets.s3.us-west-1.amazonaws.com/`;
+    : `https://gp-bermuda-reports-datasets.s3.us-east-1.amazonaws.com/`;
 
 export const eezObjective = 0.2; // 20 percent
 
