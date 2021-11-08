1d03e8ae47a94af4f3a0ed97d9823d71a5ebd21d
Tim Welch <tim.j.welch@gmail.com>
2021-11-08 13:25:22 -0800

    habitatRestore: update to latest files

--- data/habitatRestore_config.sh
+++ data/habitatRestore_config.sh
@@ -2,6 +2,6 @@
 
 ## declare an array variable
 declare -a LAYERS=(
-  "seagrass_restoration"
-  "mangrove_saltmarsh_restoration"
+  "seagrass_restoration_500mbuff"
+  "mangrove_restoration_500mbuff"
 )
