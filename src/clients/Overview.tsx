2b2d5cb5797d3aa0354b72faa8f0c3f0094529ac
Tim Welch <tim.j.welch@gmail.com>
2021-11-08 00:00:13 -0800

    Add draft InfoStatus

--- src/clients/Overview.tsx
+++ src/clients/Overview.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent } from "react";
-import { SketchAttributesCard } from "@seasketch/geoprocessing/client";
+import { SketchAttributesCard, Card } from "@seasketch/geoprocessing/client";
 import SizeCard from "./SizeCard";
 import ProtectionCard from "./ProtectionCard";
 import ExistingProtections from "./ExistingProtections";
+import { InfoStatus } from "../components/InfoStatus";
 
 interface ReportProps {
   hidden: boolean;
@@ -11,6 +12,19 @@ interface ReportProps {
 const Overview: FunctionComponent<ReportProps> = ({ hidden }) => {
   return (
     <div style={{ display: hidden ? "none" : "block" }}>
+      <Card>
+        <InfoStatus
+          size={32}
+          msg={
+            <span>
+              These are <b>draft</b> reports. Users will be notified of
+              significant changes via SeaSketch forums post for which users
+              should receive an email. Direct all report questions, requests and
+              feedback to PERSON@gmail.com
+            </span>
+          }
+        />
+      </Card>
       <SizeCard />
       <ProtectionCard />
       <ExistingProtections />
