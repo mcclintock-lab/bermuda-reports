b10701973f7ea0a7a515ee93c8440d8dd1b8327a
Tim Welch <tim.j.welch@gmail.com>
2022-01-18 22:20:38 -0800

    ✨ publish_all script

---
+++ data/_publish_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./existingProtections-2-publish.sh
+./habitat-2-publish.sh
+./habitatNursery-2-publish.sh
+./habitatRestore-2-publish.sh
+./nearshore-2-publish.sh
+./oceanuse-2-publish.sh
+./platformEdge-2-publish.sh
+./reefindex-2-publish.sh
+./renewable-2-publish.sh
+./shipwreck-2-publish.sh
