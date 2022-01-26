9885e47ec649deaac826f9b581bec2408595da96
Tim Welch <tim.j.welch@gmail.com>
2022-01-25 19:53:42 -0800

    🐛 fix types test

--- src/metrics/types.test.ts
+++ src/metrics/types.test.ts
@@ -1,50 +1,45 @@
-import { ExtendedSketchMetric } from "./types";
+import { createMetric } from "./helpers";
+import { Metric } from "./types";
 
-const areaMetrics: ExtendedSketchMetric[] = [
-  // Collection-level metrics, by class
-  {
-    reportId: "report1",
+// Collection-level metrics, by class
+const areaMetrics: Metric[] = [
+  createMetric({
     sketchId: "616f1ae906f1b7772b51f190",
     metricId: "area",
     value: 129760135.88214482,
     classId: "eez",
-  },
-  {
-    reportId: "report1",
+  }),
+  createMetric({
     sketchId: "616f1ae906f1b7772b51f190",
     metricId: "area_perc",
     value: 0.0002786123691900815,
     classId: "eez",
-  },
-  {
-    reportId: "report1",
+  }),
+  createMetric({
     sketchId: "616f1ae906f1b7772b51f190",
     metricId: "area",
     value: 129760135.88214482,
     classId: "nearshore",
-  },
-  {
-    reportId: "report1",
+  }),
+  createMetric({
     sketchId: "616f1ae906f1b7772b51f190",
     metricId: "area_perc",
     value: 0.050598639699453905,
     classId: "nearshore",
-  },
+  }),
   // Collection sketch-level metrics, by class
-  {
-    reportId: "report1",
+  createMetric({
     sketchId: "616f1ae906f1b7772b51f18f",
     metricId: "area",
     value: 98813282.51586914,
     classId: "eez",
-  },
-  {
-    reportId: "report1",
+  }),
+  createMetric({
     sketchId: "616f1ae906f1b7772b51f18f",
     metricId: "area_perc",
     value: 0.00021216533538620785,
     classId: "eez",
-  },
+  }),
 ];
 
 describe("metricTypes", () => {
